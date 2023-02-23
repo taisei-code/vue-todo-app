bcd1fdbee23720473cdeced967d094e04e57d63b
Taisei <taisei96.web@gmail.com>
2023-02-23 11:42:57 +0900

    ボタンのイベント処理

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const app = Vue.createApp({
   data: () => ({
-    message: 'Hello'
-  })
+   
+  }),
+  methods: {
+    addItem: function(event) {
+      console.log('Clicked!')
+    }
+  }
 })
 app.mount('#app')
